37ecc515c17d429e361da517b4b10d17c5b58f8e
Node.4 <islomsaparov908@gmail.com>
2026-09-18 17:11:02 +0500

    Fix JS syntax in layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,28 +3,22 @@ import RegisterModal from "./components/RegisterModal/page";
 import NavBar from "./components/NavBar/page";
 import { Providers } from "./providers";
 import Footer from "./components/Footer/page";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
 import { WishlistProvider } from "./context/WishlistContext";
 import PreloaderWrapper from "./components/PreloaderWrapper";
 
-export default async function RootLayout({ children }) {
-  const messages = await getMessages();
-
+export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body>
         <PreloaderWrapper />
-        <NextIntlClientProvider messages={messages}>
-          <Providers>
-            <WishlistProvider>
-              <NavBar />
-              <RegisterModal />
-              {children}
-              <Footer />
-            </WishlistProvider>
-          </Providers>
-        </NextIntlClientProvider>
+        <Providers>
+          <WishlistProvider>
+            <NavBar />
+            <RegisterModal />
+            {children}
+            <Footer />
+          </WishlistProvider>
+        </Providers>
       </body>
     </html>
   );
